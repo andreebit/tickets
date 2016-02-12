d5e86d6268f7c209469090661d5310f332d46e3a
Andreé Sandoval Rojas <andree@MacBook-Air-de-Andree.local>
2016-02-12 07:59:04 -0500

    usuario por defecto

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -11,6 +11,14 @@ class UserTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $user = [
+            'name' => 'Juan Pérez',
+            'email' => 'juanperez@upc.edu.pe',
+            'password' => \Hash::make('123456'),
+            'created_at' => date('Y-m-d H:i:s')
+        ];
+        
+        DB::table('users')->delete();
+        DB::table('users')->insert($user);
     }
 }
